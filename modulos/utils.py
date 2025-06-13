f1bf6e5be148a7a20beed7a3507fcbe63ab3a8f2
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-13 16:00:44 -0600

    Return de la función leer_archivo modificada para utilizar la función strip en cada linea del archivo

--- modulos/utils.py
+++ modulos/utils.py
@@ -5,5 +5,8 @@ def leer_archivo(ruta_archivo):
     try:
         with open(ruta_archivo, "r", encoding="utf-8") as archivo:
             contenido = archivo.read()
-        return contenido
+        return [linea.strip() for linea in archivo.readlines() if linea.strip()]
+    except FileNotFoundError:
+        print(f"La información solicitada no se encontró. Verifique la ruta: {ruta_archivo}")
+        return None
 
