7f304cd89e8a3b37b4483847c5495f206a2799cc
zCarlosAM <caaguirre@uamv.edu.ni>
2025-06-13 14:58:13 -0600

    Añadida la clase usuario

---
+++ modulos/usuario.py
@@ -0,0 +1,13 @@
+class Usuario:
+    def registrar(tipo):
+        pass
+    def asignar_rol():
+        pass
+    def modificar():
+        pass
+    def inactivar():
+        pass
+    def acceder():
+        pass
+    def cerrar_sesion():
+        pass
