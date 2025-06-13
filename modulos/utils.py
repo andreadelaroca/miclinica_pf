ec53ab426491dca72749d09e4116b271f1697f61
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-13 15:58:35 -0600

    Modifiqué el except de la función leer_archivo en caso de que no se encuentre el archivo

--- modulos/utils.py
+++ modulos/utils.py
@@ -2,4 +2,8 @@
 
 # función para leer un archivo de texto
 def leer_archivo(ruta_archivo):
-    
+    try:
+        with open(ruta_archivo, "r", encoding="utf-8") as archivo:
+            contenido = archivo.read()
+        return contenido
+
