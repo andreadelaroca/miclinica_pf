591909dd7c925e072da0f98be17e78986c927f10
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-06-29 20:39:22 -0600

    Gestor de archivos DAT

--- modulos/utils.py
+++ modulos/utils.py
@@ -1,4 +1,14 @@
 # funciones comunes utilizadas en el sistema
 
+import os
+from datetime import datetime
+from typing import List, Dict, Any, Optional
 
+class FileManager:
+    # gestor simple de archivos DAT
+    def __init__(self, data_dir: str = "data"):
+        self.data_dir = data_dir
+        self.separador = "|"
+        self._crear_directorio()
+        
     
